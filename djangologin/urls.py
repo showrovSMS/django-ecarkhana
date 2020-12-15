a39692449b1925b1474cfe3d472713d62f491c43
showrov <shakirshowrov26692@gmail.com>
2020-12-15 18:35:17 +0600

    add bike and bicycle images

--- djangologin/urls.py
+++ djangologin/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
 
-from djangologin.views import abc
+from djangologin.views import abc, abc2, abc3
 
 urlpatterns = [
     path('home/', abc, name="home"),
+    path('bike/', abc2, name="bike"),
+    path('bicycle/', abc3, name="bicycle"),
 
 ]
--- djangologin/views.py
+++ djangologin/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def abc(request):
 
-    return render(request,"index.html",{'question_object':"",'option_objects': ""})
+    return render(request,"front end/index.html",{'question_object':"",'option_objects': ""})
+
+def abc2(request):
+
+    return render(request,"front end/bike-index.html",{'question_object':"",'option_objects': ""})
+
+def abc3(request):
+
+    return render(request,"front end/bicycle-index.html",{'question_object':"",'option_objects': ""})
