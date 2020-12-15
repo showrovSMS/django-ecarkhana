4df7655dbdc014b1762b27b3c9186ea8084d77f4
showrov <shakirshowrov26692@gmail.com>
2020-12-15 17:35:59 +0600

    url

--- ecarkhanalogin/urls.py
+++ ecarkhanalogin/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 from ecarkhanalogin import settings
 
 urlpatterns = [
-    path('admin/admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('djangologin.urls')),
     path('car-page/', include('car.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
