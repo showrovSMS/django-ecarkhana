74ab37d911760a25e9398315b0572c4d7afa9b0e
Shakir <shakirshowrov26692@gmail.com>
2020-12-26 23:32:17 +0600

    model updates e carkhana

--- djangologin/admin.py
+++ djangologin/admin.py
@@ -3,49 +3,91 @@ from django.contrib import admin
 # Register your models here.
 from djangologin.models import *
 
+#
+# @admin.register(Prices)
+# class AdminPrices(admin.ModelAdmin):
+#     list_display = [f.name for f in Prices._meta.fields]
 
-@admin.register(Prices)
-class AdminPrices(admin.ModelAdmin):
-    list_display = [f.name for f in Prices._meta.fields]
+
+#e-carkhana
+
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    list_display = [f.name for f in User._meta.fields]
+
+@admin.register(Guest)
+class AdminGuest(admin.ModelAdmin):
+    list_display = [f.name for f in Guest._meta.fields]
 
 
-@admin.register(Suppliers)
-class AdminSuppliers(admin.ModelAdmin):
-    list_display = [f.name for f in Suppliers._meta.fields]
-
+@admin.register(Roles)
+class AdminRoles(admin.ModelAdmin):
+    list_display = [f.name for f in Roles._meta.fields]
 
 @admin.register(Product)
 class AdminProduct(admin.ModelAdmin):
     list_display = [f.name for f in Product._meta.fields]
 
-@admin.register(Categories)
-class AdminCategories(admin.ModelAdmin):
-    list_display = [f.name for f in Categories._meta.fields]
 
+@admin.register(Cart)
+class AdminCart(admin.ModelAdmin):
+    list_display = [f.name for f in Cart._meta.fields]
 
-@admin.register(Customers)
-class AdminCustomers(admin.ModelAdmin):
-    list_display = [f.name for f in Customers._meta.fields]
+@admin.register(Cart_item)
+class AdminCart_item(admin.ModelAdmin):
+    list_display = [f.name for f in Cart_item._meta.fields]
 
-@admin.register(CustomersCustomersdemo)
-class AdminCustomersCustomersdemo(admin.ModelAdmin):
-    list_display = [f.name for f in CustomersCustomersdemo._meta.fields]
-
-@admin.register(Customersdemographics)
-class AdminCustomersdemographics(admin.ModelAdmin):
-    list_display = [f.name for f in Customersdemographics._meta.fields]
 
 @admin.register(Orders)
 class AdminOrders(admin.ModelAdmin):
     list_display = [f.name for f in Orders._meta.fields]
 
-@admin.register(Ordersdetails)
-class AdminOrdersdetails(admin.ModelAdmin):
-    list_display = [f.name for f in Ordersdetails._meta.fields]
+@admin.register(Orders_status)
+class AdminOrders_status(admin.ModelAdmin):
+    list_display = [f.name for f in Orders_status._meta.fields]
 
-@admin.register(Shipper)
-class AdminShipper(admin.ModelAdmin):
-    list_display = [f.name for f in Shipper._meta.fields]
+
+@admin.register(General)
+class AdminGeneralr(admin.ModelAdmin):
+    list_display = [f.name for f in General._meta.fields]
+
+
+@admin.register(Product_categori)
+class AdminProduct_categori(admin.ModelAdmin):
+    list_display = [f.name for f in Product_categori._meta.fields]
+
+
+@admin.register(Review)
+class AdminReview(admin.ModelAdmin):
+    list_display = [f.name for f in Review._meta.fields]
+
+
+@admin.register(Traffic)
+class AdminTraffic(admin.ModelAdmin):
+    list_display = [f.name for f in Traffic._meta.fields]
+
+
+@admin.register(Blog)
+class AdminBlog(admin.ModelAdmin):
+    list_display = [f.name for f in Blog._meta.fields]
+
+
+
+@admin.register(Message)
+class AdminMessage(admin.ModelAdmin):
+    list_display = [f.name for f in Message._meta.fields]
+
+
+
+@admin.register(Reply_message)
+class AdminReply_message(admin.ModelAdmin):
+    list_display = [f.name for f in Reply_message._meta.fields]
+
+
+
+@admin.register(Home_slider)
+class AdminHome_slider(admin.ModelAdmin):
+    list_display = [f.name for f in Home_slider._meta.fields]
 
 
 
--- djangologin/models.py
+++ djangologin/models.py
@@ -2,106 +2,276 @@ from django.db import models
 
 # Create your models here.
 
+#
+# class Prices(models.Model):
+#
+#     previous_selling_price = models.DecimalField("previous Selling Price", max_digits=25, decimal_places=2, null=True, blank=True)
+#     discount = models.IntegerField("Discount", null=True, blank=True, default=0)
+#     buying_price = models.DecimalField("Buying Price", max_digits=25, decimal_places=2, null=True, blank=True)
+#     selling_price = models.IntegerField("Selling Price", null=True, blank=True)
+#     image_url = models.TextField("Image URL", null=True, blank=True)
+#     timestamp = models.DateTimeField("Timestamp", null=True, blank=True, auto_now_add=True)
+#
+#
+#     class Meta:
+#         verbose_name_plural = "Prices"
+#     def _str_(self):
+#         return str(self.id)
 
-class Prices(models.Model):
+#user
 
-    previous_selling_price = models.DecimalField("previous Selling Price", max_digits=25, decimal_places=2, null=True, blank=True)
-    discount = models.IntegerField("Discount", null=True, blank=True, default=0)
-    buying_price = models.DecimalField("Buying Price", max_digits=25, decimal_places=2, null=True, blank=True)
-    selling_price = models.IntegerField("Selling Price", null=True, blank=True)
-    image_url = models.TextField("Image URL", null=True, blank=True)
-    timestamp = models.DateTimeField("Timestamp", null=True, blank=True, auto_now_add=True)
+class User(models.Model):
 
-
-    class Meta:
-        verbose_name_plural = "Prices"
-    def _str_(self):
-        return str(self.id)
-
-#customer
-
-class Customers(models.Model):
-
-    customers_id = models.IntegerField(null=True, blank=True)
-    company_name = models.TextField(null=True, blank=True)
-    contact_name = models.TextField(null=True, blank=True)
-    contact_title = models.TextField(null=True, blank=True)
-    address = models.TextField(null=True, blank=True)
-    city = models.TextField(null=True, blank=True)
-    postal_code = models.TextField(null=True, blank=True)
-    region = models.TextField(null=True, blank=True)
+    user_id = models.IntegerField(null=True, blank=True)
+    user_name = models.TextField(null=True, blank=True)
+    user_email = models.EmailField(null=True, blank=True)
+    current_password = models.TextField(null=True, blank=True)
+    new_password = models.TextField(null=True, blank=True)
     country = models.TextField(null=True, blank=True)
-    phone = models.TextField(null=True, blank=True)
-    fax = models.TextField(null=True, blank=True)
-
-    class Meta:
-        verbose_name_plural = "Customers"
-    def _str_(self):
-        return str(self.id)
-
-
-class CustomersCustomersdemo(models.Model):
-
-    customers_id =  models.ForeignKey(Customers, null=True, blank=True, on_delete=models.DO_NOTHING)
-    customer_type = models.TextField(null=True, blank=True)
-
-    class Meta:
-        verbose_name_plural = "CustomersCustomersdemo"
-    def _str_(self):
-        return str(self.id)
-
-
-
-class Customersdemographics(models.Model):
-
-    customers_id =  models.ForeignKey(CustomersCustomersdemo, null=True, blank=True, on_delete=models.DO_NOTHING)
-    customer_type = models.TextField(null=True, blank=True)
-
-    class Meta:
-        verbose_name_plural = "Customersdemographics"
-    def _str_(self):
-        return str(self.id)
-
-
-
-#suppliers
-
-class Suppliers(models.Model):
-
-    suppliers_id = models.IntegerField(null=True, blank=True)
-    company_name = models.TextField(null=True, blank=True)
-    contact_name = models.TextField(null=True, blank=True)
-    contact_title = models.TextField(null=True, blank=True)
+    image_url = models.TextField(null=True, blank=True)
+    phone = models.IntegerField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
-    city = models.TextField(null=True, blank=True)
-    postal_code = models.TextField(null=True, blank=True)
-    region = models.TextField(null=True, blank=True)
+    billing_address = models.TextField(null=True, blank=True)
+    shipping_address = models.TextField(null=True, blank=True)
+    registrated_at_date = models.TextField(null=True, blank=True)
+    login_at_date = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "User"
+    def _str_(self):
+        return str(self.id)
+
+
+#guest
+
+class Guest(models.Model):
+
+    guest_id = models.IntegerField(null=True, blank=True)
+    guest_name = models.TextField(null=True, blank=True)
+    guest_email = models.EmailField(null=True, blank=True)
+    current_password = models.TextField(null=True, blank=True)
+    new_password = models.TextField(null=True, blank=True)
     country = models.TextField(null=True, blank=True)
-    phone = models.TextField(null=True, blank=True)
-    fax = models.TextField(null=True, blank=True)
-    homepage = models.TextField(null=True, blank=True)
-
-
-    class Meta:
-        verbose_name_plural = "Suppliers"
-    def _str_(self):
-        return str(self.id)
-
-#shipper
-
-class Shipper(models.Model):
-
-    shipper_id = models.IntegerField(null=True, blank=True)
-    company_name = models.TextField(null=True, blank=True)
-    phone = models.TextField(null=True, blank=True)
+    image_url = models.ImageField(null=True, blank=True)
+    phone = models.IntegerField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    billing_address = models.TextField(null=True, blank=True)
+    shipping_address = models.TextField(null=True, blank=True)
+    registrated_at_date = models.TextField(null=True, blank=True)
+    login_at_date = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = "Shipper"
+        verbose_name_plural = "Guest"
     def _str_(self):
         return str(self.id)
 
 
 
+
+#roles
+
+class Roles(models.Model):
+
+    roles_id = models.IntegerField(null=True, blank=True)
+    user_id =  models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
+    name = models.TextField(null=True, blank=True)
+    type = models.TextField(null=True, blank=True)
+    permission = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Roles"
+    def _str_(self):
+        return str(self.id)
+
+
+
+#traffic
+
+class Traffic(models.Model):
+
+    traffic_id = models.IntegerField(null=True, blank=True)
+    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
+    ip = models.IntegerField(null=True, blank=True)
+    latitude = models.DecimalField(max_digits=25, decimal_places=2, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=25, decimal_places=2, null=True, blank=True)
+    browser = models.TextField(null=True, blank=True)
+    browser_version = models.DecimalField(max_digits=25, decimal_places=2, null=True, blank=True)
+    platform = models.TextField(null=True, blank=True)
+    device = models.TextField(null=True, blank=True)
+    visited_page = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Traffic"
+    def _str_(self):
+        return str(self.id)
+
+
+
+
+
+
+# product
+
+class Product(models.Model):
+
+    product_id = models.IntegerField(null=True, blank=True)
+    sku = models.TextField(null=True, blank=True)
+    product_name = models.TextField(null=True, blank=True)
+    product_des = models.TextField(null=True, blank=True)
+    img_url = models.TextField(null=True, blank=True)
+    product_price = models.TextField(null=True, blank=True)
+    product_type = models.TextField(null=True, blank=True)
+    product_category = models.TextField(null=True, blank=True)
+    product_cart_details = models.TextField(null=True, blank=True)
+    product_available = models.TextField(null=True, blank=True)
+    product_stock = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+    deleted_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Product"
+    def _str_(self):
+        return str(self.id)
+
+
+
+#product_review+comment
+
+class Review(models.Model):
+
+    review_id = models.IntegerField(null=True, blank=True)
+    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
+    comments_box = models.TextField(null=True, blank=True)
+    review_box = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+
+
+    class Meta:
+        verbose_name_plural = "Review"
+
+
+    def _str_(self):
+        return str(self.id)
+
+
+
+# product_categories
+
+class Product_categori(models.Model):
+
+    cat_id = models.IntegerField(null=True, blank=True)
+    cate_name = models.TextField(null=True, blank=True)
+    cate_slug = models.TextField(null=True, blank=True)
+    img_url = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+    deleted_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Product_categori"
+    def _str_(self):
+        return str(self.id)
+
+
+#general_specification
+
+
+class General(models.Model):
+
+    general_id = models.IntegerField(null=True, blank=True)
+    bike_type = models.TextField(null=True, blank=True)
+    brand = models.TextField(null=True, blank=True)
+    model = models.TextField(null=True, blank=True)
+    displacement = models.TextField(null=True, blank=True)
+    regis_year = models.IntegerField(null=True, blank=True)
+    kms_driven = models.IntegerField(null=True, blank=True)
+    no_of_gear = models.IntegerField(null=True, blank=True)
+    weight = models.TextField(null=True, blank=True)
+    made_of_origin = models.TextField(null=True, blank=True)
+    color = models.TextField(null=True, blank=True)
+    condition = models.TextField(null=True, blank=True)
+    engine_type = models.TextField(null=True, blank=True)
+    max_power = models.TextField(null=True, blank=True)
+    ground_clereance = models.TextField(null=True, blank=True)
+    suspension = models.TextField(null=True, blank=True)
+    fueL_tank_capacity = models.TextField(null=True, blank=True)
+    brake_system = models.TextField(null=True, blank=True)
+    kerb_weight = models.TextField(null=True, blank=True)
+    top_speed = models.TextField(null=True, blank=True)
+    bore = models.TextField(null=True, blank=True)
+    stroke = models.TextField(null=True, blank=True)
+    max_torque = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "General"
+    def _str_(self):
+        return str(self.id)
+
+
+
+#cart
+
+class Cart(models.Model):
+
+    cart_id = models.IntegerField(null=True, blank=True)
+    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+    deleted_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Cart"
+    def _str_(self):
+        return str(self.id)
+
+#cart_item
+
+class Cart_item(models.Model):
+
+    cart_item_id = models.IntegerField(null=True, blank=True)
+    product_id = models.ForeignKey(Product, null=True, blank=True, on_delete=models.DO_NOTHING)
+    cart_id = models.ForeignKey(Cart, null=True, blank=True, on_delete=models.DO_NOTHING)
+    sku = models.TextField(null=True, blank=True)
+    price = models.TextField(null=True, blank=True)
+    discount = models.TextField(null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+    deleted_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Cart_item"
+    def _str_(self):
+        return str(self.id)
+
+#orders_status
+
+class Orders_status(models.Model):
+
+    order_status_id = models.IntegerField(null=True, blank=True)
+    pending_payment = models.TextField(null=True, blank=True)
+    failed = models.TextField(null=True, blank=True)
+    processing = models.TextField(null=True, blank=True)
+    completed = models.TextField(null=True, blank=True)
+    on_hold = models.TextField(null=True, blank=True)
+    cancelled = models.TextField(null=True, blank=True)
+    refunded = models.TextField(null=True, blank=True)
+    authentication_required = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Orders_status"
+    def _str_(self):
+        return str(self.id)
 
 
 #orders
@@ -109,25 +279,19 @@ class Shipper(models.Model):
 class Orders(models.Model):
 
     order_id = models.IntegerField(null=True, blank=True)
-    customer_id = models.ForeignKey(Customers, null=True, blank=True, on_delete=models.DO_NOTHING)
-    employee_id = models.IntegerField(null=True, blank=True)
-    order_date = models.DateTimeField(null=True, blank=True)
-    required_date = models.DateTimeField(null=True, blank=True)
-    shipped_date = models.DateTimeField(null=True, blank=True)
-    shipped_via = models.ForeignKey(Shipper, null=True, blank=True, on_delete=models.DO_NOTHING)
-    freight = models.DecimalField(null=True, blank=True, max_digits=25, decimal_places=2)
-    ship_name = models.TextField(null=True, blank=True)
-    ship_address = models.TextField(null=True, blank=True)
-    ship_city = models.TextField(null=True, blank=True)
-    ship_region = models.TextField(null=True, blank=True)
-    ship_postal_code = models.TextField(null=True, blank=True)
-    ship_country = models.TextField(null=True, blank=True)
+    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
+    cart_id = models.ForeignKey(Cart, null=True, blank=True, on_delete=models.DO_NOTHING)
+    session_id = models.IntegerField(null=True, blank=True)
+    order_status_id = models.ForeignKey(Orders_status, null=True, blank=True, on_delete=models.DO_NOTHING)
+    order_status = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+    deleted_at_date = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Orders"
     def _str_(self):
         return str(self.id)
-
 
 
 # categories
@@ -136,7 +300,9 @@ class Categories(models.Model):
     category_id = models.IntegerField(null=True, blank=True)
     category_name = models.TextField(null=True, blank=True)
     description= models.TextField(null=True, blank=True)
-    picture = models.ImageField(null=True, blank=True)
+    img_url = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -146,24 +312,75 @@ class Categories(models.Model):
 
 
 
-    # product
+# blog
 
-class Product(models.Model):
-
-    product_id = models.IntegerField(null=True, blank=True)
-    supplier_id = models.ForeignKey(Suppliers, null=True, blank=True, on_delete=models.DO_NOTHING)   #Foregeien key
-    category_id =  models.ForeignKey(Categories, null=True, blank=True, on_delete=models.DO_NOTHING)   #Foregeien key
-    product_name = models.TextField(null=True, blank=True)
-    quantity_per_unit = models.TextField(null=True, blank=True)
-    unit_price = models.DecimalField(null=True, blank=True, max_digits=25, decimal_places=2)
-    units_in_stoke = models.IntegerField(null=True, blank=True)
-    units_in_order = models.IntegerField(null=True, blank=True)
-    reorder_level = models.IntegerField(null=True, blank=True)
-
-
+class Blog(models.Model):
+    blog_id = models.IntegerField(null=True, blank=True)
+    blog_name = models.TextField(null=True, blank=True)
+    blog_des = models.TextField(null=True, blank=True)
+    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
+    img_url = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = "Product"
+        verbose_name_plural = "Blog"
+
+    def _str_(self):
+        return str(self.id)
+
+
+
+# Home_slider
+
+class Home_slider(models.Model):
+    slider_id = models.IntegerField(null=True, blank=True)
+    device = models.TextField(null=True, blank=True)
+    title = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    img_url = models.TextField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Home_slider"
+
+    def _str_(self):
+        return str(self.id)
+
+
+# message_box
+
+class Message(models.Model):
+    mess_id = models.IntegerField(null=True, blank=True)
+    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
+    text_box = models.TextField(null=True, blank=True)
+    time = models.DateTimeField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Message"
+
+    def _str_(self):
+        return str(self.id)
+
+
+
+
+# Reply_message_box
+
+class Reply_message(models.Model):
+    reply_id = models.IntegerField(null=True, blank=True)
+    message_id = models.ForeignKey(Message, null=True, blank=True, on_delete=models.DO_NOTHING)
+    text_box = models.TextField(null=True, blank=True)
+    time = models.DateTimeField(null=True, blank=True)
+    created_at_date = models.TextField(null=True, blank=True)
+    updated_at_date = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Reply_message"
+
     def _str_(self):
         return str(self.id)
 
@@ -171,17 +388,5 @@ class Product(models.Model):
 
 
 
-#order details
 
-class Ordersdetails(models.Model):
 
-    order_id =  models.ForeignKey(Orders, null=True, blank=True, on_delete=models.DO_NOTHING)
-    product_id =  models.ForeignKey(Product, null=True, blank=True, on_delete=models.DO_NOTHING)
-    quantity_id = models.IntegerField(null=True, blank=True)
-    unit_price_decimel = models.DecimalField(null=True, blank=True, max_digits=25, decimal_places=2)
-    discount_double = models.DecimalField(null=True,  blank=True, max_digits=25, decimal_places=2)
-
-    class Meta:
-        verbose_name_plural = "Ordersdetails"
-    def _str_(self):
-        return str(self.id)
